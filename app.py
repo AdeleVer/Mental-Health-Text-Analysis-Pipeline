21a98627ecb6aaffa631921c3a95486e2a303e18
AdeleVer <Adeleveremeow@gmail.com>
2025-10-19 21:43:33 +0500

    Add gunicorn and Procfile for deployment

--- app.py
+++ app.py
@@ -249,5 +249,5 @@ if __name__ == '__main__':
         db.create_all()
         logger.info("Database tables created!")
     
-    logger.info("Starting server...")
+    logger.info("Starting development server...")
     app.run(debug=True, host='0.0.0.0', port=5000)
